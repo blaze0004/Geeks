3c562fcd06da900a4f8c4afad00927f6dcb9d418
Haris Tyagi <tyagiharis370@gmail.com>
2018-05-27 13:37:59 +0530

    codethenext

---
+++ competition/codethenext2018/bobeditor.cpp
@@ -0,0 +1,53 @@
+/*
+
+   Bob and Editor
+
+Bob is working on his college project. He tried to make an editor like Vim, but he came up with some messed editor which will follow only following commands :
+
+dd  -- delete the current line and move the cursor to the end of next line. If there is no next line, move the cursor to end of the previous line. If all the lines are deleted, move the cursor to the start of the first line.
+
+:m  -- move the cursor to the end of line m
+
+i m -- each following m lines contain text to insert.
+
+y -- copy the current line and replace the content of the buffer with the copied line.
+
+p -- paste the content of the buffer (the copied line) at the end of the current line and move the cursor to the end of the line. If no line is copied, ignore this command.
+
+dy -- it will work similar to command dd and also copy the deleted line and replace the content of the buffer with the copied line.
+
+You are given some initial text T and Q commands. You are required to print the final text after applying the given commands on T. Initially, the cursor is present at the end of the last line and the buffer is empty.
+
+Input :
+
+The first line of input contains an integer N, number of lines in the initial text T.
+
+Next N lines contain the content of T.
+
+Next line contains Q, denoting the number of commands.
+
+It is followed by Q commands.
+
+Output :
+
+Print the final text T after applying the Q commands.
+
+
+*/
+
+
+#include<iostream.h>
+#include<string.h>
+using namespace std;
+class Editor {
+
+    private:
+        
+        int n,q;
+        string t, newt;
+        int currentIndex;
+        
+    
+        
+
+}
