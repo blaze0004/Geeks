6785ff2cb08aa8854301b438449bafcf8c6b246f
blaze0004 <tyagiharis370@gmail.com>
2019-08-05 00:28:06 +0530

    new graph bfs dfs solution

---
+++ datastructures/Graph/Intro to dfs and bfs/bidirectional-search.py
@@ -0,0 +1,14 @@
+
+from Graph import UnDirectedGraph
+
+class BidirectionalSearch(UnDirectedGraph):
+
+	def __init__(self):
+
+		UnDirectedGraph.__init__(self)
+
+	def bidirectionalSearch(self, src, dest):
+
+		pass
+
+	
--- datastructures/Graph/Intro to dfs and bfs/count-nodes-within-k-distance-from-all-nodes-in-a-set.py
+++ datastructures/Graph/Intro to dfs and bfs/count-nodes-within-k-distance-from-all-nodes-in-a-set.py
@@ -1,90 +1,109 @@
 
 from Graph import UnDirectedGraph
+import sys
 
 class KDistance(UnDirectedGraph):
 
 	def __init__(self):
 		UnDirectedGraph.__init__(self)
 
-	def countNodesAtKDistFromMarked(self, marked, k):
-
-		prevVisited = [0] * (self.V)
-		#self.BFS(marked, i, k, visited)
-		visited = []
-		for mark in marked:
-
-			visited = [0] * self.V
-
-			visited[mark] += 0
-			
-
-			queue = [mark]
-
-			while queue:
-
-				v = queue.pop(0)
-
-				# if prevVisited[v] and prevVisited[v] > k:
-				# 	visited[v] = prevVisited[v]
-
-				for i in self.graph[v]:
-
-					if not visited[i]:
-
-						visited[i] += visited[v] + 1
-
-						queue.append(i)
-
-					# if visited[i] > k:
-					# 	visited[i] = prevVisited[i]
-				# if visited[v] > k:
-				# 	visited[v] = prevVisited[v]
-			prevVisited = visited
-
-		for i in map(lambda x: x, visited):
-			print(i, end = ' ')
-
-	def BFS(self, marked, i, k, visited):
-
-		src = marked[i]
-		visited[src] += 1
+	def BFS(self, src, marked, findMinMax = False):
 
 		queue = [src]
+		currV = [0] * (self.V)
+
+		currV[src] = 1
 
 		while queue:
 
-			s = queue.pop(0)
+			v = queue.pop(0)
 
-			if visited[s] > k:
-				break
+			for i in self.graph[v]:
 
-			visited[s] = 0
+				if self.visited[i] > k+1:
+					currV[i] = self.visited[i]
+					self.visited[i] = 0
+					queue.append(i)
+					continue
 
-			for v in self.graph[s]:
+				if not currV[i]:
 
-				if not visited[v]:
+					currV[i] = currV[v] + 1
+					queue.append(i)
+		# print(currV)
+		self.visited = currV
+		#print(list(map(lambda x: x-1, self.visited)))
+		if findMinMax:
+			a = -1
+			v = 0
+			for m in marked:
 
-					visited[v] = visited[s] + 1
-					queue.append(v)
+				if a < currV[m] - 1:
+					a = currV[m] - 1
+					v = m
 
-		self.BFS(marked, i+1, k, visited)
+			a = sys.maxsize
+			u = 0 
+			for m in marked:
+
+				if a > currV[m] - 1:
+					a = currV[m] - 1
+					u = m
+
+			return [u, v]
+
+		del currV
+		return self.visited
+
+
+
+	def countNodesAtKDistFromMarked(self, marked, k):
+
+		self.visited = [0] * (self.V)
+
+		# First BFS to find max distances
+		u, v = self.BFS(0, marked, True)
+
+		# if u == 0:
+		# 	self.BFS(v, marked)
+		# elif v == 0:
+		# 	self.BFS(u, marked)
+		# else:
+		self.visited = [0] * (self.V)
+		visited1 = self.BFS(u, marked)
+		self.visited = [0] * (self.V)
+		visited2 = self.BFS(v, marked)
+
+		nodeCount = 0
+		#print(self.visited)
+		for i in range(self.V):
+			if visited1[i]-1 <= k and visited2[i]-1 <= k:
+				nodeCount += 1
+
+		del self.visited
+		del visited1, visited2, u, v
+		return nodeCount
 
 graph = KDistance()
 
 graph.addEdge(1, 0)
 graph.addEdge(0, 3)
 graph.addEdge(0, 8)
+graph.addEdge(10, 11)
+graph.addEdge(11, 3)
+graph.addEdge(11, 5)
 graph.addEdge(2, 3)
-graph.addEdge(3, 5)
+# graph.addEdge(3, 5)
 graph.addEdge(3, 6)
 graph.addEdge(3, 7)
 graph.addEdge(4, 5)
 graph.addEdge(5, 9)
 
-marked = [1,2, 4]
+marked = [1, 2, 4, 10]
 k = 3
-graph.countNodesAtKDistFromMarked(marked, k)
-
+nodeCount = graph.countNodesAtKDistFromMarked(marked, k)
+print(nodeCount)
 # [1, 0, 3, 2, 4, 3, 3, 3, 2, 4]
+# [3, 4, 3, 2, 4, 1, 2, 2, 4, 4]
 # [2, 3, 0, 1, 4, 2, 2, 2, 3, 4] [3, 3, 3, 3, 8, 5, 5, 5, 5, 8]
 # [3, 4, 3, 2, 4, 1, 3, 3, 4, 4] [6, 7, 6, 5, 12, 6, 8, 8, 9, 12] [3, 4, 3, 2, 9, 3, 5, 5, 6, 9]
